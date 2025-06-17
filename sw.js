1fffbe76dbdef0c355df0f8fd9585c174562c4bd
DVT <army4hyungs@gmail.com>
2025-06-17 15:48:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIza...",
+  authDomain: "yourproject.firebaseapp.com",
+  projectId: "yourproject",
+  storageBucket: "yourproject.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcd1234"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const { title, body, icon } = payload.notification;
+  self.registration.showNotification(title, { body, icon });
+});
+
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
